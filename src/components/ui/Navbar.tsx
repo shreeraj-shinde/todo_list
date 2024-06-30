19a7521cb9798fe1f508dac4908d676d74296faf
shreeraj-shinde <shreerajshinde86@gmail.com>
2024-06-30 07:16:02 -0700

    Project Completed

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="px-40 p-4 bg-purple-700">
+    <div className="lg:px-40 p-4 bg-purple-700">
       <h1 className="text-white font-semibold tracking-wide uppercase text-xl">
         Todo List
       </h1>
